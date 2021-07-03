b4836119c26f4278c4c48ee5d479f255d4fad437
Aseer KT <aseermiqdad@gmail.com>
2021-07-03 17:42:03 +0530

    feat: protect middleware

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,7 +1,9 @@
 import passport from 'passport';
+import express from 'express';
 import { CLIENT_URL } from '../config/constants';
+import protect from '../middlewares/protect';
 
-const router = require('express').Router();
+const router = express.Router();
 
 router.get('/github', passport.authenticate('github'));
 
@@ -9,5 +11,9 @@ router.get(
   '/github/callback',
   passport.authenticate('github', { successRedirect: `${CLIENT_URL}` }),
 );
+
+router.get('/me', protect, (req, res) => {
+  res.json(req.user);
+});
 
 export default router;
